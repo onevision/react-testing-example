341447490f309f27badc165513806f34019be296
Richard Hensman <richard.hensman@onevisionconsulting.co.uk>
2022-11-29 14:43:16 +0000

    First commit

---
+++ cypress/e2e/incrementor.cy.ts
@@ -0,0 +1,15 @@
+describe('Incrementor', () => {
+  it('Should increases the count when the increment button is pressed', () => {
+    cy.visit('http://127.0.0.1:5173/')
+    cy.findByRole('button', {name: "Increment"}).click()
+    cy.findByRole('current-count').contains("Count is 1")
+  })
+
+  it('Should set the count to zero when the reset button is pressed', () => {
+    cy.visit('http://127.0.0.1:5173/')
+    cy.findByRole('button', {name: "Reset"}).click()
+    cy.findByRole('current-count').contains("Count is 0")
+  })
+})
+
+export {}
---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Incrementor</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Increment
+        </button>
+        <button onClick={() => setCount(0)}>
+          Reset
+        </button>        
+        <p role="current-count">
+          Count is {count}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
