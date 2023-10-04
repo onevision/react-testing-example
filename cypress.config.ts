43f22d9c9c5114e8dff1bb1b6e6df6a5c92b952f
Richard Hensman <richard.hensman@onevisionconsulting.co.uk>
2023-10-04 18:19:48 +0100

    Added cypress-mochawesome-reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "cypress";
+import cypressMochawesomePlugin from "cypress-mochawesome-reporter/plugin";
 
 export default defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      cypressMochawesomePlugin(on);
     },
   },
 });
