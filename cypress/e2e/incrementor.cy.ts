dfcb1af17ca31029cc90e3131b44ca51e327ffc8
Richard Hensman <richard.hensman@onevisionconsulting.co.uk>
2023-10-26 19:37:08 +0100

    Updated tests to use localhost

--- cypress/e2e/incrementor.cy.ts
+++ cypress/e2e/incrementor.cy.ts
@@ -1,15 +1,15 @@
-describe('Incrementor', () => {
-  it('Should increase the count when the increment button is pressed', () => {
-    cy.visit('http://127.0.0.1:5173/')
-    cy.findByRole('button', {name: "Increment"}).click()
-    cy.findByRole('current-count').contains("Count is 1")
-  })
+describe("Incrementor", () => {
+  it("Should increase the count when the increment button is pressed", () => {
+    cy.visit("http://localhost:5173/");
+    cy.findByRole("button", { name: "Increment" }).click();
+    cy.findByRole("current-count").contains("Count is 1");
+  });
 
-  it('Should set the count to zero when the reset button is pressed', () => {
-    cy.visit('http://127.0.0.1:5173/')
-    cy.findByRole('button', {name: "Reset"}).click()
-    cy.findByRole('current-count').contains("Count is 0")
-  })
-})
+  it("Should set the count to zero when the reset button is pressed", () => {
+    cy.visit("http://localhost:5173/");
+    cy.findByRole("button", { name: "Reset" }).click();
+    cy.findByRole("current-count").contains("Count is 0");
+  });
+});
 
-export {}
+export {};
